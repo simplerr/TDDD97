25207e3bd0d0dd1c0b2a7f55c8691e1299ece869
ockomock <oscfr819@student.liu.se>
2016-01-20 17:03:26 +0100

    dude dis be an init commit bro

---
+++ client.js
@@ -0,0 +1,10 @@
+
+displayView = function() {
+	// whatever
+}
+
+window.onload = function() {
+	// code that is executed as the page loads
+	// you shall put your own sutom code here
+	window.alert("Hello Mr cheeze Cracker");
+}
