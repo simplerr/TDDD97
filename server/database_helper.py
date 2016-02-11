be5098b2c0a5e9b8b152c33c222becdf420fc667
Axel Blackert <axebl383@student.liu.se>
2016-02-11 15:37:48 +0100

    Implemented helper functions

--- server/database_helper.py
+++ server/database_helper.py
@@ -1,32 +1,58 @@
 import sqlite3
 from flask import g
-
+from server import app
 
 def connect_db():
 	return sqlite3.connect("database.db")
 	
+def close():
+	get_db().close()
+
 def get_db():
-	db = getattr(g, 'db', None)
+	db = getattr(g, '_database', None)
 	if db is None:
-		db = g.db = connect_db()
-	return db
-	
-def init():
-	db = get_db()
-	db.execute("drop table if exists entries")
-	db.execute("create table entries (id integer primary key, name text,message text)")
-	db.commit()
+		db = g._database = connect_db()
+	return db	
 	
 def init_db():
-    with closing(connect_db()) as db:
+    with app.app_context():
+        db = get_db()
         with app.open_resource('database.schema', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+
+# from http://flask.pocoo.org/docs/0.10/patterns/sqlite3/
+def query_db(query, args=(), one=False):
+        cur = get_db().execute(query, args)
+        rv = cur.fetchall()
+        cur.close()
+        get_db().commit()
+        return (rv[0] if rv else None) if one else rv
 	
-def add_message(name,message):
-	db = get_db()
-	db.execute("insert into entries (name,message) values (?,?)", (name,message))
-	db.commit()
+def add_user(email, firstname, familyname, country, city, gender, password):
+        return query_db("INSERT INTO Users(email, firstname, familyname, country, city, gender, password) VALUES(?,?,?,?,?,?,?)", [email, firstname, familyname, country, city, gender, password])
+       
+
+def add_message(sender, receiver, message):
+	return query_db("INSERT INTO Messages(sender,receiver, message) VALUES (?,?,?)", [sender, receiver, message])
 	
-def close():
-	get_db().close()
+def get_users():
+        return query_db("SELECT * FROM Users")
+
+def get_user_messages(email):
+        return query_db("SELECT * FROM Messages WHERE receiver=?", [email])
+
+def get_user_data(email):
+        return query_db("SELECT * FROM Users WHERE email=?", [email])
+
+def set_password(email, password):
+        return query_db("UPDATE Users SET password=? WHERE email=?", [password, email])
+
+def valid_password(email, password):
+        result = query_db("SELECT * FROM Users WHERE email=? AND password=?", [email, password], one=True)
+        
+        if result is None:
+                return False
+        else:
+                return True
+        
--- server/server.py
+++ server/server.py
@@ -1,15 +1,51 @@
 import sqlite3
+import database_helper
 
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
- return 'Hello World! test'
+ return 'Hello World!'
  
-@app.route('/bye')
+@app.route('/init_db')
 def bye_world():
- return 'Bye World!'
+ database_helper.init_db()
+ return 'init_db()'
+
+@app.route('/add_message')
+def add_message():
+ database_helper.add_message("Axel", "axel.blackert@gmail.com", "Test message")
+ return 'add_message()'
+
+@app.route('/add_user')
+def add_user():
+ database_helper.add_user("__1__axel.blackert@gmail.com", "Axel", "Blackert", "Sweden", "Lkpg", "Man", "pwd12")
+ return 'add_user()'
+
+@app.route('/get_users')
+def get_users():
+ return jsonify(data = database_helper.get_users())
+
+@app.route('/get_user_messages')
+def get_user_message() :
+ return jsonify(data = database_helper.get_user_messages("axel.blackert@gmail.com"))
+
+@app.route('/get_user_data')
+def get_user_data() :
+ return jsonify(data = database_helper.get_user_data("axel.blackert@gmail.com"))
+
+@app.route('/set_password')
+def set_password() :
+ return jsonify(data = database_helper.set_password("axel.blackert@gmail.com", "nya pwd123"))
+
+@app.route('/valid_password')
+def valid_password() :
+ return jsonify(data = database_helper.valid_password("axel.blackert@gmail.com", "nya pwd123"))
+
+
 
 if __name__ == '__main__':
+ app.debug = True
  app.run() 
